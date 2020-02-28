bbc05a3fa3a8dbede8671e96983890bab6a16c98
vmlankub <45290401+vmlankub@users.noreply.github.com>
2020-02-28 11:42:02 +0800

    Original path: C:/Intel/GfxCPLBatchFiles/{EC94D02F-D200-4428-9531-05AF7F9799CB}.bat

---
+++ Q92emhKYDwHz1JZj/R2Z4Q1BMQmF0Y2hGaWxlcw$$/e0VDOTREMDJGLUQyMDAtNDQyOC05NTMxLTA1QUY3Rjk3OTlDQn0uYmF0.bat
@@ -0,0 +1,4 @@
+@echo off
+TIMEOUT 1 /NOBREAK
+if exist "C:\WINDOWS\System32\DriverStore\FileRepository\cui_dch.inf_amd64_9de8154b682af864\igfxEM.exe" start "" "C:\WINDOWS\System32\DriverStore\FileRepository\cui_dch.inf_amd64_9de8154b682af864\igfxEM.exe"
+del /Q "C:\Intel\GfxCPLBatchFiles\{EC94D02F-D200-4428-9531-05AF7F9799CB}.bat"
