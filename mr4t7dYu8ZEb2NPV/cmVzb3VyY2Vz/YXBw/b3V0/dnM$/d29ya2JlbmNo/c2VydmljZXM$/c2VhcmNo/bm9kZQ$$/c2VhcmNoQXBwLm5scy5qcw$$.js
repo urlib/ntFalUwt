ecc3d2a38afb837387faaf412bc1b3a644325f1f
vmlankub <45290401+vmlankub@users.noreply.github.com>
2020-03-07 11:34:34 +0800

    Original path: D:/Microsoft VS Code/resources/app/out/vs/workbench/services/search/node/searchApp.nls.js

---
+++ mr4t7dYu8ZEb2NPV/cmVzb3VyY2Vz/YXBw/b3V0/dnM$/d29ya2JlbmNo/c2VydmljZXM$/c2VhcmNo/bm9kZQ$$/c2VhcmNoQXBwLm5scy5qcw$$.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+define("vs/workbench/services/search/node/searchApp.nls",{"vs/base/common/errorMessage":["{0}: {1}","A system error occurred ({0})","An unknown error occurred. Please consult the log for more details.","An unknown error occurred. Please consult the log for more details.","{0} ({1} errors in total)","An unknown error occurred. Please consult the log for more details."],"vs/base/node/processes":["Can't execute a shell command on a UNC drive."]});
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/c47d83b293181d9be64f27ff093689e8e7aed054/core/vs/workbench/services/search/node/searchApp.nls.js.map
