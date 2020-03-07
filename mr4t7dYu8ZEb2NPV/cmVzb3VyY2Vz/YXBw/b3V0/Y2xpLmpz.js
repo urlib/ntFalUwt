6f07b5c158f8e4ee789cd3932fe78e104427950b
vmlankub <45290401+vmlankub@users.noreply.github.com>
2020-03-07 11:27:33 +0800

    Original path: D:/Microsoft VS Code/resources/app/out/cli.js

---
+++ mr4t7dYu8ZEb2NPV/cmVzb3VyY2Vz/YXBw/b3V0/Y2xpLmpz.js
@@ -0,0 +1,6 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+"use strict";const bootstrap=require("./bootstrap");bootstrap.avoidMonkeyPatchFromAppInsights(),bootstrap.configurePortable(),bootstrap.enableASARSupport(),require("./bootstrap-amd").load("vs/code/node/cli");
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/c47d83b293181d9be64f27ff093689e8e7aed054/core/cli.js.map
