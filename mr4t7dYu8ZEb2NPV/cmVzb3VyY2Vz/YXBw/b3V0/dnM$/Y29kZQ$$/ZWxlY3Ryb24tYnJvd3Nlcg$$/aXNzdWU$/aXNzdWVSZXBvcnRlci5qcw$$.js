5ac070e8fadb500a8ea9141cebf4a415bad3401b
vmlankub <45290401+vmlankub@users.noreply.github.com>
2020-03-07 11:29:13 +0800

    Original path: D:/Microsoft VS Code/resources/app/out/vs/code/electron-browser/issue/issueReporter.js

---
+++ mr4t7dYu8ZEb2NPV/cmVzb3VyY2Vz/YXBw/b3V0/dnM$/Y29kZQ$$/ZWxlY3Ryb24tYnJvd3Nlcg$$/aXNzdWU$/aXNzdWVSZXBvcnRlci5qcw$$.js
@@ -0,0 +1,6 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+"use strict";const bootstrapWindow=require("../../../../bootstrap-window");bootstrapWindow.load(["vs/code/electron-browser/issue/issueReporterMain"],(function(o,e){o.startup(e)}),{forceEnableDeveloperKeybindings:!0,disallowReloadKeybinding:!0});
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/c47d83b293181d9be64f27ff093689e8e7aed054/core/vs/code/electron-browser/issue/issueReporter.js.map
