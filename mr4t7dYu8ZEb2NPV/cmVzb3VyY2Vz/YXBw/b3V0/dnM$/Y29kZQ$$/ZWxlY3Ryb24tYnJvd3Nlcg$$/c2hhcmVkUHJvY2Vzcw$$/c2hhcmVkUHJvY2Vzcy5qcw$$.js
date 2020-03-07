13b4c905733c14361e693d4644089d57d29b8439
vmlankub <45290401+vmlankub@users.noreply.github.com>
2020-03-07 11:29:57 +0800

    Original path: D:/Microsoft VS Code/resources/app/out/vs/code/electron-browser/sharedProcess/sharedProcess.js

---
+++ mr4t7dYu8ZEb2NPV/cmVzb3VyY2Vz/YXBw/b3V0/dnM$/Y29kZQ$$/ZWxlY3Ryb24tYnJvd3Nlcg$$/c2hhcmVkUHJvY2Vzcw$$/c2hhcmVkUHJvY2Vzcy5qcw$$.js
@@ -0,0 +1,6 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+"use strict";const bootstrap=require("../../../../bootstrap"),bootstrapWindow=require("../../../../bootstrap-window");bootstrap.avoidMonkeyPatchFromAppInsights(),bootstrapWindow.load(["vs/code/electron-browser/sharedProcess/sharedProcessMain"],(function(o,t){o.startup({machineId:t.machineId,windowId:t.windowId})}));
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/c47d83b293181d9be64f27ff093689e8e7aed054/core/vs/code/electron-browser/sharedProcess/sharedProcess.js.map
