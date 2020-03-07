2cfcd575aa030768aa13448905cb920311c2675b
vmlankub <45290401+vmlankub@users.noreply.github.com>
2020-03-07 11:29:48 +0800

    Original path: D:/Microsoft VS Code/resources/app/out/vs/code/electron-browser/processExplorer/processExplorerMain.nls.js

---
+++ mr4t7dYu8ZEb2NPV/cmVzb3VyY2Vz/YXBw/b3V0/dnM$/Y29kZQ$$/ZWxlY3Ryb24tYnJvd3Nlcg$$/cHJvY2Vzc0V4cGxvcmVy/cHJvY2Vzc0V4cGxvcmVyTWFpbi5ubHMuanM$.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+define("vs/code/electron-browser/processExplorer/processExplorerMain.nls",{"vs/code/electron-browser/processExplorer/processExplorerMain":["CPU %","Memory (MB)","pid","Name","Kill Process","Force Kill Process","Copy","Copy All","Debug"]});
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/c47d83b293181d9be64f27ff093689e8e7aed054/core/vs/code/electron-browser/processExplorer/processExplorerMain.nls.js.map
