569be100238ed02ded7afc754faa48940da921d2
vmlankub <45290401+vmlankub@users.noreply.github.com>
2020-03-07 11:31:51 +0800

    Original path: D:/Microsoft VS Code/resources/app/out/vs/platform/files/node/watcher/unix/watcherApp.nls.js

---
+++ mr4t7dYu8ZEb2NPV/cmVzb3VyY2Vz/YXBw/b3V0/dnM$/cGxhdGZvcm0$/ZmlsZXM$/bm9kZQ$$/d2F0Y2hlcg$$/dW5peA$$/d2F0Y2hlckFwcC5ubHMuanM$.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+define("vs/platform/files/node/watcher/unix/watcherApp.nls",{"vs/base/node/processes":["Can't execute a shell command on a UNC drive."],"vs/platform/files/common/files":["Unknown Error"]});
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/c47d83b293181d9be64f27ff093689e8e7aed054/core/vs/platform/files/node/watcher/unix/watcherApp.nls.js.map
